88e6e515328bb5c219a75aec1ac130cdbcb8ad37
Dies00 <909090gaming@gmail.com>
2023-01-27 23:16:03 +0300

    Update smsbomber.py

--- smsbomber.py
+++ smsbomber.py
@@ -43,7 +43,8 @@ while 1:
 █▀▀ █▀▄▀█ █▀▀   █▀▀▄ █▀▀█ █▀▄▀█ █▀▀▄ █▀▀ █▀▀█
 ▀▀█ █░▀░█ ▀▀█   █▀▀▄ █░░█ █░▀░█ █▀▀▄ █▀▀ █▄▄▀
 ▀▀▀ ▀░░░▀ ▀▀▀   ▀▀▀░ ▀▀▀▀ ▀░░░▀ ▀▀▀░ ▀▀▀ ▀░▀▀
-                 {}  Discord: Dies#2986  
+                 {}  Discord: Dies#2986
+Uygulama şaka amaçlıdır kurulum yapıldıktan sonra tüm sorumluluk kullanıcıya aittir
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), len(servisler_call), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
         menu = int(input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder\n 2- Ara\n 3- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
